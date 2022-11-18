7cb9be1cffd84922ded515b673f0f80339c2edb7
Your Name <noon.fitzgerald.com>
2022-11-18 12:47:59 -0500

    AddEventlistener

---
+++ index.js
@@ -0,0 +1,10 @@
+
+
+const menuButton = document.querySelector('.stripes-button');
+const navBar= document.querySelector('.link-bar');
+
+        menuButton.addEventListener( "click", function() {
+            navBar.classList.toggle('mobile-button');
+        });
+
+    
